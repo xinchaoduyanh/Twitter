27a1b4f8eb42fdaaabd1dfa4ff50d7f71ebad924
Vu Duy Anh <duyanh19122k3@gmail.com>
2023-10-13 21:26:33 +0700

    Wrap UploadImageController and filter Image only

--- src/controllers/media.controller.ts
+++ src/controllers/media.controller.ts
@@ -1,20 +1,10 @@
 import { NextFunction, Request, Response } from 'express'
 import path from 'path'
+import { handleUploadSingleImage } from '~/utils/files'
 
 export const uploadSingleImageController = async (req: Request, res: Response, next: NextFunction) => {
-  const formidable = (await import('formidable')).default
-  const form = formidable({
-    uploadDir: path.resolve('uploads'), // thư mục chứa file trên server
-    maxFiles: 1,
-    keepExtensions: true, //
-    maxFileSize: 300 * 1024 // 300KB
-  })
-  form.parse(req, (err, fields, files) => {
-    if (err) {
-      throw err
-    }
-    res.json({
-      message: 'upload image success1'
-    })
+  const data = await handleUploadSingleImage(req, res, next)
+  return res.json({
+    res: data
   })
 }
--- src/utils/files.ts
+++ src/utils/files.ts
@@ -1,3 +1,4 @@
+import { NextFunction, Request, Response } from 'express'
 import fs from 'fs'
 import path from 'path'
 export const initFolerUpload = () => {
@@ -7,4 +8,35 @@ export const initFolerUpload = () => {
       recursive: true // tạo thư mục cha nếu cha chưa tồn tại
     })
   }
+}
+export const handleUploadSingleImage = async (req: Request, res: Response, next: NextFunction) => {
+  const formidable = (await import('formidable')).default
+  const form = formidable({
+    uploadDir: path.resolve('uploads'), // thư mục chứa file trên server
+    maxFiles: 1,
+    keepExtensions: true, //
+    maxFileSize: 300 * 1024,
+    filter: function ({ name, originalFilename, mimetype }) {
+      const valid = 'images' === name && Boolean(mimetype?.includes('image/'))
+      if (!valid) {
+        form.emit('error' as any, new Error('File type is not valid') as any)
+      }
+      return valid
+    } // 300KB
+  })
+  return new Promise((resolve, reject) => {
+    form.parse(req, (err, fields, files) => {
+      console.log('files', files)
+      console.log('fields', fields)
+      console.log('err', err)
+
+      if (err) {
+        return reject(err)
+      }
+      if (!files.images) {
+        return reject(new Error('File is not valid'))
+      }
+      resolve(files)
+    })
+  })
 }
