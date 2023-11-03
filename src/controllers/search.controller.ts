2b4c7c89117eb4498299aeb09171c883054204a7
Vu Duy Anh <duyanh19122k3@gmail.com>
2023-11-03 13:17:43 +0700

    tweet update view after search

--- src/controllers/search.controller.ts
+++ src/controllers/search.controller.ts
@@ -2,12 +2,13 @@ import { Request, Response } from 'express'
 import { SearchQuery } from '~/models/requests/Search.request'
 import { searchService } from '~/services/search.services'
 import { ParamsDictionary } from 'express-serve-static-core'
+import { TokenPayload } from '~/models/requests/User.requests'
 export const SearchController = async (req: Request<ParamsDictionary, any, any, SearchQuery>, res: Response) => {
   const { content } = req.query
   const limit = Number(req.query.limit)
   const page = Number(req.query.page)
-
-  const result = await searchService.search({ limit, page, content: req.query.content })
+  const user_id = req.decoded_authorization?.user_id as string
+  const result = await searchService.search({ limit, page, content: req.query.content, user_id })
   console.log(result)
 
   return res.json({
--- src/services/search.services.ts
+++ src/services/search.services.ts
@@ -1,15 +1,244 @@
 import { SearchQuery } from '~/models/requests/Search.request'
 import databaseService from './database.services'
+import { ObjectId } from 'mongodb'
+import Tweet from '~/models/schemas/Tweet.schemas'
+import { TweetType } from '~/constants/enums'
 
 class SearchService {
-  async search(query: { page: number; limit: number; content: string }) {
-    const result = await databaseService.tweets
-      .find({ $text: { $search: query.content } })
-      .skip((query.page - 1) * query.limit)
-      .limit(query.limit)
-      .toArray()
-
-    return result
+  async search({ page, limit, content, user_id }: { page: number; limit: number; content: string; user_id: string }) {
+    const [tweets, total] = await Promise.all([
+      databaseService.tweets
+        .aggregate([
+          {
+            $match: {
+              $text: {
+                $search: content
+              }
+            }
+          },
+          {
+            $lookup: {
+              from: 'users',
+              localField: 'user_id',
+              foreignField: '_id',
+              as: 'author'
+            }
+          },
+          {
+            $unwind: {
+              path: '$author'
+            }
+          },
+          {
+            $match: {
+              $or: [
+                {
+                  audience: 0
+                },
+                {
+                  $and: [
+                    {
+                      audience: 1
+                    },
+                    {
+                      'author.twitter_circle': {
+                        $in: [new ObjectId(user_id)]
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+          },
+          {
+            $lookup: {
+              from: 'users',
+              localField: 'mentions',
+              foreignField: '_id',
+              as: 'mentions'
+            }
+          },
+          {
+            $addFields: {
+              mentions: {
+                $map: {
+                  input: '$mentions',
+                  as: 'mention',
+                  in: {
+                    _id: '$$mention._id',
+                    name: '$$mention.name',
+                    username: '$$mention.username',
+                    email: '$$mention.email'
+                  }
+                }
+              }
+            }
+          },
+          {
+            $lookup: {
+              from: 'bookmarks',
+              localField: '_id',
+              foreignField: 'tweet_id',
+              as: 'bookmarks'
+            }
+          },
+          {
+            $lookup: {
+              from: 'likes',
+              localField: '_id',
+              foreignField: 'tweet_id',
+              as: 'likes'
+            }
+          },
+          {
+            $lookup: {
+              from: 'tweets',
+              localField: '_id',
+              foreignField: 'parent_id',
+              as: 'tweets_children'
+            }
+          },
+          {
+            $addFields: {
+              bookmarks_count: {
+                $size: '$bookmarks'
+              },
+              likes_count: {
+                $size: '$likes'
+              },
+              retweet_count: {
+                $size: {
+                  $filter: {
+                    input: '$tweets_children',
+                    as: 'item',
+                    cond: {
+                      $eq: ['$$item.type', TweetType.Retweet]
+                    }
+                  }
+                }
+              },
+              comment_count: {
+                $size: {
+                  $filter: {
+                    input: '$tweets_children',
+                    as: 'item',
+                    cond: {
+                      $eq: ['$$item.type', TweetType.Comment]
+                    }
+                  }
+                }
+              },
+              quote_count: {
+                $size: {
+                  $filter: {
+                    input: '$tweets_children',
+                    as: 'item',
+                    cond: {
+                      $eq: ['$$item.type', TweetType.QuoteTweet]
+                    }
+                  }
+                }
+              }
+            }
+          },
+          {
+            $project: {
+              tweets_children: 0,
+              author: {
+                password: 0,
+                email_verify_token: 0,
+                date_of_birth: 0,
+                created_at: 0,
+                updated_at: 0,
+                forgot_password_token: 0,
+                verify: 0
+              }
+            }
+          },
+          {
+            $skip: (page - 1) * limit
+          },
+          {
+            $limit: limit
+          }
+        ])
+        .toArray(),
+      databaseService.tweets
+        .aggregate([
+          {
+            $match: {
+              $text: {
+                $search: content
+              }
+            }
+          },
+          {
+            $lookup: {
+              from: 'users',
+              localField: 'user_id',
+              foreignField: '_id',
+              as: 'author'
+            }
+          },
+          {
+            $unwind: {
+              path: '$author'
+            }
+          },
+          {
+            $match: {
+              $or: [
+                {
+                  audience: 0
+                },
+                {
+                  $and: [
+                    {
+                      audience: 1
+                    },
+                    {
+                      'author.twitter_circle': {
+                        $in: [new ObjectId(user_id)]
+                      }
+                    }
+                  ]
+                }
+              ]
+            }
+          },
+          {
+            $count: 'total'
+          }
+        ])
+        .toArray()
+    ])
+    //Update view cho tweet sau khi dc search
+    const ids = tweets.map((tweet) => tweet._id)
+    const date = new Date()
+    await databaseService.tweets.updateMany(
+      {
+        _id: {
+          $in: ids
+        }
+      },
+      {
+        $set: {
+          updated_at: date
+        },
+        $inc: {
+          user_views: 1
+        }
+      }
+    )
+    //Cap nhat data tra ve cho khach hang
+    tweets.forEach((tweet) => {
+      tweet.updated_at = date
+      tweet.user_views += 1
+    })
+    return {
+      tweets,
+      total
+    }
   }
 }
 
